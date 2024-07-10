7c47f024c25041d0c2dd061baef6aa72b59265ef
Daniel Leon Silva <dleonsilva7226@gmail.com>
2024-07-10 13:34:52 -0400

    Create Playing Screen

---
+++ JPongLauncher.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+//DON'T HARDCODE VALUES
+import Screens.PlayingScreen;
+public class JPongLauncher {
+
+    public static void main (String[] args) {
+        PlayingScreen newGame = new PlayingScreen();
+        
+    }
+}
---
+++ Screens/PlayingPanel.java
@@ -0,0 +1,7 @@
+package Screens;
+//Draw Rectangles from Here
+// public class PlayingPanel extends JPanel {
+//     public PlayingPanel () {
+//         this.
+//     }
+// }

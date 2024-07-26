bec60f9d53560791879a9c364fc1364edaa2b3a1
Daniel Leon Silva <dleonsilva7226@gmail.com>
2024-07-25 22:55:13 -0400

    Add Dummy Screens to Create Pause Feature

---
+++ utilz/Screens.java
@@ -0,0 +1,14 @@
+package utilz;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Screens {
+    public static final String PAUSE_IMG = "res/paused_screen.png";
+    public static final String INSTRUCTIONS_IMG = "res/instructions_screen.png";
+    public static final String START_IMG = "res/start_screen.png";
+    public static final String GAME_OVER_IMG = "res/game_over_screen.png";
+
+    public static final Image PAUSE_SCREEN = new ImageIcon(PAUSE_IMG).getImage();
+}
